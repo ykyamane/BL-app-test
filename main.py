769e35478807fe1d9040b65baeac6f086d81746b
ykyamane <ykyamane@gmail.com>
2025-08-30 17:50:48 +0900

    update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-import nibabel as nib
+#import nibabel as nib
 import os
 
 def main(input_file, output_file):
-    img = nib.load(input_file)
-    header_str = str(img.header)
-
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    #img = nib.load(input_file)
+    #header_str = str(img.header)
+    header_str='test-test-test'
+    os.makedirs(os.path.dirname(output_file))
     with open(output_file, 'w') as f:
         f.write(header_str)
     print(output_file)
