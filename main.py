be830107c033ae5fbfc920929f521b3d4e02bee6
ykyamane <ykyamane@gmail.com>
2025-09-01 12:11:55 +0900

    Update main.py
    modified

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python
-
 import sys
-#import nibabel as nib
-import os
 
-def main(input_file, output_file):
-    #img = nib.load(input_file)
-    #header_str = str(img.header)
-    header_str='test-test-test'
-    os.makedirs(os.path.dirname(output_file))
-    with open(output_file, 'w') as f:
-        f.write(header_str)
-    print(output_file)
 
-if __name__ == "__main__":
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-    main(input_file, output_file)
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(output_file, "w", encoding="utf-8") as file:
+    file.write(input_file)
